d7174e4f105014fd134d4121d7ee1e9f5ab6f876
Elvis Oliveira <elvis.olv@gmail.com>
2023-06-09 13:07:55 +0200

    Google lighthouse optimizations

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ npm run dev
 # to publish, just run the deploy script and push
 npm run deploy
 ```
+
+```
+let photos = [];
+document.querySelectorAll("a[href*='/p/']").forEach((a) => {
+	photos.push({
+		href: a.href,
+		img: a.querySelector("img").alt,
+		src: a.querySelector("img").src
+	});
+});
+console.log(JSON.stringify(photos));
+```
--- dest/gallery.js
+++ dest/gallery.js
@@ -1,20 +1,11 @@
 $(function() {
-  var container, containerWidth, loaded, pictures, slider, sliderWidth;
-  container = $('div#portifolio-gallery');
-  containerWidth = container.innerWidth();
-  slider = container.find('ul');
-  sliderWidth = 0;
-  pictures = slider.find('img');
-  loaded = 0;
-  pictures.on('load', function() {
-    sliderWidth += $(this).width();
-    loaded++;
-    if (loaded === pictures.length) {
-      $('div#portifolio-gallery').mousemove(function(e) {
-        slider.css({
-          left: ((containerWidth - sliderWidth) * (e.pageX / containerWidth).toFixed(3)).toFixed(1) + 'px'
-        });
-      });
-    }
+  return $('div#portifolio-gallery ul').slick({
+    arrows: false,
+    lazyLoad: 'ondemand',
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    variableWidth: true,
+    swipeToSlide: true
   });
 });
